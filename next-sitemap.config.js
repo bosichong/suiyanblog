48e6a51a2682ae369af17c9794691dc5d392af09
J.sky <bosichong@qq.com>
2024-12-30 19:05:02 +0800

    更新

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-// next-sitemap.config.js
-module.exports = {
-    siteUrl: 'https://www.suiyan.cc', // 替换为你的网站URL
-    generateRobotsTxt: true, // 是否生成robots.txt
-    sitemapSize: 7000, // 每个sitemap文件的URL数量
-    // 其他sitemap配置...
-};
-
