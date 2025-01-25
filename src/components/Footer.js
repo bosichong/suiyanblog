870f61dc671e33fa28cb607347e4b1643c4b4422
J.sky <bosichong@qq.com>
2025-01-25 21:30:59 +0800

    更新

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,13 @@ import {Link} from "@nextui-org/react";
 
 export default function Footer (){
     return(
-        <footer className={'container max-w-3xl mx-auto leading-normal text-lg font-extralight'}>
+        <footer className={'container max-w-3xl mx-auto leading-normal font-extralight'}>
             <div className={'mx-auto max-w-5xl p-2'}>
                 <div className={'mx-auto mt-4 max-w-md text-center text-sm '}>&copy; 2017 - 2025 <Link
-                    className={'text-center text-sm'} href="https://www.suiyan.cc">SuiYan 碎言博客</Link> Built with <Link href="https://nextjs.org/">Next.js</Link></div>
+                    className={'text-center text-sm'} href="https://www.suiyan.cc">SuiYan 碎言博客</Link></div>
+                <div className={'mx-auto mt-4 max-w-md text-center text-sm'}>
+                    Built with <Link href="https://nextjs.org/">Next.js</Link> Powered by <Link href="https://nextjs.org/">Vercel</Link>
+                    </div>
             </div>
 
         </footer>
