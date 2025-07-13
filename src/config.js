0713720c8abe562ebd380229685ff27ed58519f4
J.sky <bosichong@qq.com>
2025-07-13 21:52:57 +0800

    更新

--- src/config.js
+++ src/config.js
@@ -34,10 +34,10 @@ const config = {
     ],
     "links": [
         {
-            "site_name": "堆新",
-            "site_url": "https://duinew.com/",
-            "site_description": "堆行成路•新途无界（DUINEW）",
-            "site_avatar": "https://d.duinew.com/logo.webp"
+            "site_name": "八对星星",
+            "site_url": "https://8dui.com",
+            "site_description": "极目星视穹苍无界•足履行者大地有疆",
+            "site_avatar": "https://cf.8dui.com/logo.webp"
         },
         {
             "site_name": "二丫讲梵",
