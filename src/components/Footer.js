691a3a1843c4f440d9e11e08862379a8ebfef65f
J.sky <bosichong@qq.com>
2025-11-28 09:13:44 +0800

    更新

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,8 +9,11 @@ export default function Footer (){
                 <div className={'mx-auto mt-4 max-w-md text-center text-sm '}>&copy; 2017 - 2025 <Link
                     className={'text-center text-sm'} href="https://www.suiyan.cc">SuiYan 碎言博客</Link></div>
                 <div className={'mx-auto mt-4 max-w-md text-center text-sm'}>
-                    Built with <Link href="https://nextjs.org/">Next.js</Link> Powered by <Link href="https://vercel.com">Vercel</Link>
+                    Built with <Link href="https://nextjs.org/" target="_blank">Next.js</Link> Powered by <Link href="https://vercel.com" target="_blank">Vercel</Link>
                     </div>
+                <div>
+                    <Link href="https://follow.it/suiyan?leanpub" target="_blank"><img src="https://www.suiyan.cc/assets/images/follow.it.png" /></Link>
+                </div>
             </div>
 
         </footer>
