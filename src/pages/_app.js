7f9d788dfbf819494dcb6ec5c640bc5a56888e6c
J.sky <bosichong@qq.com>
2026-01-07 07:50:29 +0800

    更新

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import {NextUIProvider} from "@nextui-org/react";
 import {ThemeProvider as NextThemesProvider} from "next-themes";
 import {Analytics} from "@vercel/analytics/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export default function App({ Component, pageProps }) {
   return (
       <NextUIProvider>
           <NextThemesProvider attribute="class" defaultTheme="dark">
               <Component {...pageProps} />
               <Analytics />
+              <SpeedInsights />
           </NextThemesProvider>
       </NextUIProvider>
 );
