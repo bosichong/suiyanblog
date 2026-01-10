b86aa79dc7d4281899d487b8a82d7bc3f52c5930
J.sky <bosichong@qq.com>
2026-01-10 09:34:00 +0800

    更新

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -2,31 +2,34 @@
 import { Card, CardHeader, CardBody, CardFooter, Link } from '@nextui-org/react';
 import formatDate from '../utils/formatDate';
 import BlogTime from './ico/BlogTime';
+import GlowCard from './GlowCard';
 
 export default function PostCard({ post }) {
     return (
-        <Card className="my-2 hover:animate-pulse" key={post.id}>
-            <CardHeader className="flex gap-3">
-                <Link href={`/blog/${post.id}`} className="flex flex-col">
-                    <h3 className="text-2xl rainbow_text">{post.title}</h3>
-                </Link>
-            </CardHeader>
+        <GlowCard key={post.id} borderWidth={2}>
+            <Card className="hover:animate-pulse">
+                <CardHeader className="flex gap-3">
+                    <Link href={`/blog/${post.id}`} className="flex flex-col">
+                        <h3 className="text-2xl rainbow_text">{post.title}</h3>
+                    </Link>
+                </CardHeader>
 
-            <CardBody>
-                <p className="text-md">{post.description}</p>
-            </CardBody>
+                <CardBody>
+                    <p className="text-md">{post.description}</p>
+                </CardBody>
 
-            <CardFooter className="text-sm">
-                <Link href={`/blog/${post.id}`} className="block text-sm justify-start w-1/2 flex">
-                    阅读全文 <span className="block text-sm">➞</span>
-                </Link>
-                <div className="flex justify-end w-1/2">
-                    <div className="flex items-center text-sm">
-                        <BlogTime />
-                        <span className="ml-2">{formatDate(post.time)}</span>
+                <CardFooter className="text-sm">
+                    <Link href={`/blog/${post.id}`} className="block text-sm justify-start w-1/2 flex">
+                        阅读全文 <span className="block text-sm">➞</span>
+                    </Link>
+                    <div className="flex justify-end w-1/2">
+                        <div className="flex items-center text-sm">
+                            <BlogTime />
+                            <span className="ml-2">{formatDate(post.time)}</span>
+                        </div>
                     </div>
-                </div>
-            </CardFooter>
-        </Card>
+                </CardFooter>
+            </Card>
+        </GlowCard>
     );
 }
