8e64a1c4dfedfbba91f0e5620da82e02e312c686
J.sky <bosichong@qq.com>
2026-01-12 09:22:34 +0800

    更新

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ export default function Footer (){
         <footer className={'container max-w-3xl mx-auto leading-normal font-extralight'}>
             <div className={'mx-auto max-w-5xl p-2'}>
                 <div className={'mx-auto mt-4 max-w-md text-center text-sm '}>&copy; 2017 - 2026 <Link
-                    className={'text-center text-sm'} href="https://www.suiyan.cc">SuiYan 碎言博客</Link></div>
+                    className={'text-center text-sm'} href="https://www.suiyan.cc">SuiYan 碎言博客</Link>  Built with <Link href="https://nextjs.org/" target="_blank">Next.js</Link> Powered by <Link href="https://vercel.com" target="_blank">Vercel</Link></div>
                 <div className={'mx-auto mt-4 max-w-md text-center text-sm'}>
-                    Built with <Link href="https://nextjs.org/" target="_blank">Next.js</Link> Powered by <Link href="https://vercel.com" target="_blank">Vercel</Link>
+                   
                 </div>
 
                 <div className={'mx-auto mt-4 max-w-md text-center text-sm'}>
