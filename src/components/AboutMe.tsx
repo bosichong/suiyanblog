7c8f1ef3e942dcabb31d6fe228bf7c6f281c3b08
J.sky <bosichong@qq.com>
2026-01-29 18:56:58 +0800

    更新 - 2026-01-29 18:56

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,7 +2,7 @@ import config from '../config';
 
 const AboutMe = () => {
   return (
-    <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md p-6 mb-6">
+    <div className="border border-dashed border-gray-300 dark:border-gray-600 rounded-lg p-6 mb-6">
       <div className="prose dark:prose-invert max-w-none">
         <p className="text-gray-700 dark:text-gray-300 whitespace-pre-line">{config.ABOUT_ME}</p>
         <div className="mt-4 text-right">
