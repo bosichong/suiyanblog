b9cacc4f9b0db389fbcc9a99259fa9b4103c8108
J.sky <bosichong@qq.com>
2026-02-01 13:40:14 +0800

    更新 - 2026-02-01 13:40

--- src/pages/test1.tsx
+++ src/pages/test1.tsx
@@ -1,6 +1,5 @@
 import getSortedPostsData from '../utils/parseMd';
-import PostListItem from '../components/PostListItem';
-import formatDate from '../utils/formatDate';
+import Link from 'next/link';
 
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData();
@@ -8,55 +7,32 @@ export async function getStaticProps() {
     const minimalPosts = allPostsData.map(post => ({
         id: post.id,
         title: post.title,
-        time: post.time,
-        author: post.author,
     }));
-
-    const postsByYear: { [key: string]: any[] } = {};
-    minimalPosts.forEach((post) => {
-        const year = post.time ? post.time.split('-')[0] : '';
-        if (!postsByYear[year]) {
-            postsByYear[year] = [];
-        }
-        postsByYear[year].push(post);
-    });
 
     return {
         props: {
             allPostsData: minimalPosts,
-            postsByYear,
         },
     };
 }
 
-const Test1 = ({ allPostsData, postsByYear }: { allPostsData: any[]; postsByYear: { [key: string]: any[] } }) => {
+const Test1 = ({ allPostsData }: { allPostsData: any[] }) => {
     const totalPosts = allPostsData.length;
-    const lastUpdated = allPostsData[0]?.time ? formatDate(allPostsData[0].time) : '';
 
     return (
         <div className="w-full">
             <div className="mb-8">
                 <p className="text-sm text-text-secondary">
-                    共有文章：{totalPosts} 篇，最后更新于 {lastUpdated}
+                    共有文章：{totalPosts} 篇
                 </p>
             </div>
 
-            <ul className="space-y-6 no-padding-left">
-                {Object.keys(postsByYear).sort((a, b) => parseInt(b) - parseInt(a)).map((year) => (
-                    <li key={year} className="post-list-group">
-                        <p className="post-list-group-title">{year}</p>
-                        <ul className="space-y-2 no-padding-left">
-                            {postsByYear[year].map((post) => (
-                                <li key={post.id}>
-                                    <PostListItem
-                                        id={post.id}
-                                        title={post.title || ''}
-                                        time={post.time || ''}
-                                        formatDate={formatDate}
-                                    />
-                                </li>
-                            ))}
-                        </ul>
+            <ul className="space-y-2 no-padding-left">
+                {allPostsData.map((post) => (
+                    <li key={post.id}>
+                        <Link href={`/blog/${post.id}`} className="text-lg hover:text-text-dark">
+                            {post.title}
+                        </Link>
                     </li>
                 ))}
             </ul>
