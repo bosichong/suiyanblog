2431c1dd0d8703813bd9767c1ddd9d67de726957
J.sky <bosichong@qq.com>
2026-02-01 14:54:34 +0800

    更新 - 2026-02-01 14:54

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,14 +1,12 @@
 import config from '../config';
+import CustomLink from './Link';
 
 const AboutMe = () => {
   return (
     <div className="mb-8 flex justify-between items-center text-sm text-text-secondary">
-      <span>{config.ABOUT_ME}  <a 
-        href="/blog/1" 
-        className="text-blue-400 hover:text-orange-600 hover:underline font-medium"
-      >
-        关于我 →
-      </a></span>
+      <span>{config.ABOUT_ME}  <CustomLink href="https://www.suiyan.cc" underline={true}>
+                        关于我 →
+                    </CustomLink></span>
       
     </div>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,18 +5,18 @@ import SnsIcons from './SnsIcons';
 export default function Footer() {
     return (
         <footer className="mt-8 mb-8">
-            <div className="flex flex-col items-center gap-4 text-text-secondary">
+            <div className="flex flex-col items-center gap-4">
                 {/* SNS 图标 */}
                 <SnsIcons />
 
                 {/* 版权信息 */}
-                <p className="m-0 text-center text-xs text-text-tertiary">
+                <p className="m-0 text-center">
                     &copy; 2017 - 2026{' '}
-                    <CustomLink href="https://www.suiyan.cc">
+                    <CustomLink href="https://www.suiyan.cc" target='_blank' underline={true}>
                         {config.BLOG_NAME}
                     </CustomLink>
                     {' '}· Built with{' '}
-                    <CustomLink href="https://nextjs.org/" target="_blank">
+                    <CustomLink href="https://nextjs.org/" target="_blank" underline={true}>
                         Next.js
                     </CustomLink>
                 </p>
