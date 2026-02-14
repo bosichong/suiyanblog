b1e3852fa95a8bd49b23cbea427dac3730bba3c9
J.sky <bosichong@qq.com>
2026-02-14 09:21:31 +0800

    修改首页 全部文章按钮样式

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -10,11 +10,6 @@ interface PrimaryButtonProps {
 
 export default function PrimaryButton({ href, children, className = '' }: PrimaryButtonProps) {
   return (
-    <a className={`group relative inline-block overflow-hidden border border-rose-700 px-6 py-3 ${className}`} href={href}>
-      <span className="absolute inset-x-0 bottom-0 h-0.5 bg-rose-700 transition-all group-hover:h-full"></span>
-      <span className="relative text-base  text-rose-700 transition-colors group-hover:text-white">
-        {children}
-      </span>
-    </a>
+    <a className="custom-btn btn-16" href={href}>{children}</a>
   );
 }
