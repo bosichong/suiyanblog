eea35d052cc382a2bffee6389c81c3b2dc8834d3
J.sky <bosichong@qq.com>
2026-03-05 10:25:27 +0800

    更新 - 2026-03-05 10:25

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -10,7 +10,15 @@ export default function Custom404() {
                 <meta name="description" content="抱歉，您访问的页面不存在" />
                 <meta name="robots" content="noindex, follow" />
             </Head>
-            <div>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                minHeight: '100vh',
+                textAlign: 'center',
+                padding: '20px'
+            }}>
                 <div>
                     <svg width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="100" cy="100" r="90" stroke="currentColor" strokeWidth="2" />
@@ -20,12 +28,12 @@ export default function Custom404() {
                         <text x="100" y="180" textAnchor="middle" fontSize="24" fill="currentColor">404</text>
                     </svg>
                 </div>
-                <h1>页面走丢了</h1>
-                <p>
+                <h1 style={{ marginTop: '20px' }}>页面走丢了</h1>
+                <p style={{ marginTop: '15px' }}>
                     就像人生一样，有时候你也会走到死胡同。<br />
                     别担心，这只是个页面，不是你的人生。
                 </p>
-                <Link href="/">
+                <Link href="/" style={{ marginTop: '20px' }}>
                     回到首页
                 </Link>
             </div>
