e781cbf86af5a13cac0730c94b370c6b2f178751
J.sky <bosichong@qq.com>
2026-03-24 08:13:56 +0800

    更新 - 2026-03-24 08:13

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -68,6 +68,9 @@ export default function ThemeSwitcher() {
                 toggleTheme();
             }}
             data-tooltip="点击切换主题模式"
+            aria-label={`切换主题模式，当前为${getThemeLabel()}模式`}
+            role="button"
+            tabIndex={0}
         >
             {getThemeLabel()}
         </a>
