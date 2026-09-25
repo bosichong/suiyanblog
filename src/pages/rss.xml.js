a763c2052e6e8ad32b8aa85ff6dac7be70f56867
bosichong <bosichong@qq.com>
2026-09-25 18:56:54 +0800

    fix: rss.xml 复用 feed.xml 输出，保证两者内容一致

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,1 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
-
-export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `https://www.suiyan.cc/blog/${post.id}`,
-			
-		})),
-	});
-}
+export { GET } from './feed.xml.js';
